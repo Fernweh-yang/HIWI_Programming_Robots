0a892e792ea705ca3bdeee21151b82050cb92931
Fernweh-yang <512127058@qq.com>
2023-05-23 18:17:04 +0200

    :white_check_mark: test: control joint position

--- mujoco-2.3.5/task6/one_leg.cc
+++ mujoco-2.3.5/task6/one_leg.cc
@@ -1,17 +1,23 @@
-#include "mujoco/mujoco.h"
-#include "GLFW/glfw3.h"
+#include<stdbool.h> //for bool
+#include<unistd.h> //for usleep
+#include <math.h>
+#include <mujoco/mujoco.h>
+#include <GLFW/glfw3.h>
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
 
-#include <iostream>
-#include <eigen3/Eigen/Core>
-#include <eigen3/Eigen/Dense>
-#include <eigen3/Eigen/Eigen>
-using namespace Eigen;
-
 double simulation_endtime = 20;
 int step_number;
+
+typedef enum{
+  LOADING       = 0X00, // Zero hip torque
+  COMPRESSION   = 0X01, // Body attitude with hip
+  THRUST        = 0X02, // Spring give a thrust + Body attitude with hip
+  UNLOADING     = 0X03, // Zero hip torque
+  //FLIGHT        = 0X04, // position leg for loading
+}stateMachineTypeDef;
+stateMachineTypeDef stateMachine;
 
 // MuJoCo data structures
 mjModel* m = NULL;                  // MuJoCo model
@@ -28,8 +34,19 @@ bool button_right =  false;
 double lastx = 0;
 double lasty = 0;
 
-// init parameters
-VectorXd init_qpos(7);
+// holders of one step history of time and position to calculate dertivatives
+mjtNum position_history = 0;
+mjtNum previous_time = 0;
+
+// controller related variables
+float_t ctrl_update_freq = 100;
+mjtNum last_update = 0.0;
+mjtNum ctrl;
+
+
+char path[] = "../task6/franka_emika_panda/";
+char xmlfile[] = "scene.xml";
+
 
 void keyboard(GLFWwindow* window, int key, int scancode, int act, int mods)
 {
@@ -91,26 +108,72 @@ void scroll(GLFWwindow* window, double xoffset, double yoffset)
 }
 
 
-void init_controller(const mjModel* m, mjData* d)
+void set_torque(const mjModel* m,int actuator_number,int flag)
 {
-  init_qpos << 0.6579, -0.06562,-0.6566,-2.339,-0.0441,2.2911,0.7280;
-  for (size_t i=0;i<7;i++)
-   {
-    d->qpos[i] = init_qpos[i];
-  }
-
+  if (flag==0)
+    m->actuator_gainprm[10*actuator_number+0]=0;
+  else
+    m->actuator_gainprm[10*actuator_number+0]=1;
 }
 
-void init_pose(const mjModel* m, mjData* d){
-  init_qpos << 0.6579, -0.06562,-0.6566,-2.339,-0.0441,2.2911,0.7280;
-  for (size_t i=0;i<7;i++)
-  {
-    d->qpos[i] = init_qpos[i];
-  }
+void set_position(const mjModel* m,int actuator_number,double kp)
+{
+  m->actuator_gainprm[10*actuator_number+0]=kp;
+  m->actuator_biasprm[10*actuator_number+1]=-kp;
+}
+
+void set_velocity(const mjModel* m,int actuator_number,double kv)
+{
+  m->actuator_gainprm[10*actuator_number+0]=kv;
+  m->actuator_biasprm[10*actuator_number+2]=-kv;
+}
+
+
+void init_controller(const mjModel* m, mjData* d)
+{
+  int actuator_number;
+
+  actuator_number = 0; //hip_p
+  set_position(m,actuator_number,100);
+  actuator_number = 1; //hip_v
+  set_velocity(m,actuator_number,10);
+
+  actuator_number = 2; //knee_p
+  set_position(m,actuator_number,1000);
+  actuator_number = 3; //knee_v
+  set_velocity(m,actuator_number,0);
+
+  stateMachine = LOADING;
+  step_number = 0;
+
 }
 
 void update_controller(double pos_foot[3], double z_velocity){
-  
+  if (stateMachine==LOADING && pos_foot[2]<=0.05)
+  {
+    stateMachine = COMPRESSION;
+    //printf("COMPRESSION \n");
+  }
+
+  if (stateMachine==COMPRESSION && z_velocity>0)
+  {
+    stateMachine = THRUST;
+    //printf("THRUST \n");
+  }
+
+  if (stateMachine==THRUST && pos_foot[2]>=0.05)
+  {
+    stateMachine = UNLOADING ;
+    //printf("UNLOADING  \n");
+  }
+
+  if (stateMachine==UNLOADING  && z_velocity<0)
+  {
+    stateMachine = LOADING;
+    //printf("LOADING \n");
+    step_number = step_number+1;
+    printf("%d \n",step_number);
+  }
 }
 
 void controller(const mjModel* m, mjData* d)
@@ -123,27 +186,76 @@ void controller(const mjModel* m, mjData* d)
 
   //all actions
   int actuator_number;
+  if (stateMachine == LOADING)
+  {
+    actuator_number = 2; //knee_p
+    set_position(m,actuator_number,1000);
+    actuator_number = 3; //knee_v
+    set_velocity(m,actuator_number,100);
+  }
+
+  if (stateMachine == COMPRESSION)
+  {
+    actuator_number = 2; //knee_p
+    set_position(m,actuator_number,1000);
+    actuator_number = 3; //knee_v
+    set_velocity(m,actuator_number,0);
+
+
+  }
+
+  if (stateMachine == THRUST)
+  {
+    actuator_number = 2; //knee_p
+    set_position(m,actuator_number,1050);
+    actuator_number = 3; //knee_v
+    set_velocity(m,actuator_number,0);
+
+    d->ctrl[0] = -0.2;
+  }
+
+  if (stateMachine == UNLOADING )
+  { 
+    actuator_number = 2; //knee_p
+    set_position(m,actuator_number,1000);
+    actuator_number = 3; //knee_v
+    set_velocity(m,actuator_number,100);
+
+    d->ctrl[0] = 0;
+  }
 }
 
 
 int main(int argc, const char** argv)
 {
-    // ***************** start *****************
-    // ***************** load model and data *****************
-    mj_activate("mjkey.txt");                 // activate software
-    char filepath[] = "../task6/franka_emika_panda//scene.xml";
+
+    // activate software
+    mj_activate("mjkey.txt");
+    char xmlpath[100]={};
+    char datapath[100]={};
+    strcat(xmlpath,path);
+    strcat(xmlpath,xmlfile);
+
+    // load and compile model
     char error[1000] = "Could not load binary model";
-    
-    m = mj_loadXML(filepath, 0, error, 1000); // load and compile model
+
+    // check command-line arguments
+    if( argc<2 )
+        m = mj_loadXML(xmlpath, 0, error, 1000);
+
+    else
+        if( strlen(argv[1])>4 && !strcmp(argv[1]+strlen(argv[1])-4, ".mjb") )
+            m = mj_loadModel(argv[1], 0);
+        else
+            m = mj_loadXML(argv[1], 0, error, 1000);
     if( !m )
         mju_error_s("Load model error: %s", error);
 
-    d = mj_makeData(m);                       // get data of model
-    // ***************** end *****************
+    // get data of model
+    d = mj_makeData(m);
 
 
-    // ***************** start *****************
-    // ***************** render the window *****************
+    // init GLFW
     if( !glfwInit() )
         mju_error("Could not initialize GLFW");
     // create window
@@ -161,31 +273,20 @@ int main(int argc, const char** argv)
     glfwSetCursorPosCallback(window, mouse_move);
     glfwSetMouseButtonCallback(window, mouse_button);
     glfwSetScrollCallback(window, scroll);
-    // ***************** end *****************
+    // set the camera view
+    double arr_view[] = {90, -10, 5, 0.000000, 0.000000, 2.000000};
+    cam.azimuth = arr_view[0];
+    cam.elevation = arr_view[1];
+    cam.distance = arr_view[2];
+    cam.lookat[0] = arr_view[3];
+    cam.lookat[1] = arr_view[4];
+    cam.lookat[2] = arr_view[5];
 
+    // add Control
+    mjcb_control = controller;
+    init_controller(m,d);
 
-    // ***************** start *****************
-    // ***************** define the controller *****************
-    // init_pose(m,d);    
-    init_qpos << 0.6579,-0.06562,-0.6566,-2.339,-0.0441,2.2911,0.7280;
-    for (size_t i=0;i<7;i++){
-        d->qpos[i] = init_qpos[i];
-    }
-
-
-
-
-
-
-
-
-    mjcb_control = controller;  // define the control callback;
-    // init_controller(m,d);
-    // ***************** end *****************
-
-
-    // ***************** start *****************
-    // ***************** simulate *****************
+    // Simulate
     while( !glfwWindowShouldClose(window))
     {
         mjtNum simstart = d->time;
@@ -209,8 +310,8 @@ int main(int argc, const char** argv)
         mjr_render(viewport, &scn, &con);
         glfwSwapBuffers(window);
         glfwPollEvents();
+
     }
-    // ***************** end *****************
 
     mjv_freeScene(&scn);
     mjr_freeContext(&con);
